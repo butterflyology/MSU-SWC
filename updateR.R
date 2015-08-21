5bfb6d2acb22320270be7e561cfa95773767d537
Chris Hamm <chamm@ku.edu>
2015-08-21 09:34:54 -0400

    added updateR with proper dir

--- updateR.R
+++ updateR.R
@@ -1,12 +1,12 @@
 #updateR, by CA Hamm (version 0.1) 19 October 2014
 
 #--run in the old version of R
-setwd("~/")
+setwd("~/MSU-SWC")
 packages <- installed.packages()[,"Package"]
 save(packages, file="Rpackages.3.2.2")
 
 #--run in the new version
-setwd("~/")
+setwd("~/MSU-SWC")
 load("Rpackages.3.2.2")
 for (p in setdiff(packages, installed.packages()[,"Package"]))
 install.packages(p)
